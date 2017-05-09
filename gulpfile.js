806a8c25af6a7034b8d023c4699aab766914465e
Peter Ruttkay-Nedecky <pruttned@gmail.com>
2017-05-09 21:59:59 +0200

    chore(gulpfile): add css cache bust

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 'use strict';
 
 const gulp = require('gulp'),
+    del = require('del'),
+    print = require('gulp-print'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     inject = require('gulp-inject'),
-    autoprefixer = require('gulp-autoprefixer');
+    rev = require('gulp-rev'),
+    revReplace = require('gulp-rev-replace'),
+    useref = require('gulp-useref'),
+    filter = require('gulp-filter'),
+    autoprefixer = require('gulp-autoprefixer'),
+    gulpSequence = require('gulp-sequence');
 
 const srcRoot = 'src/';
+const buildRoot = '.tmp/';
+const assetsRoot = 'assets/';
 const paths = {
     rootSass: srcRoot + 'sass/site.scss',
-    sass: srcRoot + 'sass/**/*.scss',
-    assetsDest: 'assets/',
+    sass: srcRoot + 'sass/**/*.scss'
 };
 
 function isOnlyChange(event) {
@@ -18,12 +26,12 @@ function isOnlyChange(event) {
 }
 
 gulp.task('css', function () {
-    gulp.src([paths.rootSass])
+    return gulp.src([paths.rootSass])
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(gulp.dest(paths.assetsDest));
+        .pipe(gulp.dest(buildRoot));
 });
 
 gulp.task('inject:sass', function () {
@@ -41,6 +49,13 @@ gulp.task('inject:sass', function () {
         .pipe(gulp.dest(f => f.base));
 });
 
+gulp.task('clean', function () {
+    return del([
+        assetsRoot,
+        buildRoot
+    ]);
+});
+
 gulp.task('watch', function () {
     gulp.watch(paths.sass, function (event) {
         if (isOnlyChange(event)) {
@@ -51,5 +66,25 @@ gulp.task('watch', function () {
     });
 });
 
-gulp.task('build', ['css']);
+gulp.task('copy', () => {
+    gulp.src(srcRoot + '_includes/**/*')
+        .pipe(print())
+        .pipe(gulp.dest('_includes'));
+});
 
+gulp.task('build:main', ['css', 'copy']);
+gulp.task('build:post', () => {
+    let cssFilter = filter('**/*.css', { restore: true });
+    let htmlFilter = filter('**/*.html', { restore: true });
+    let assetsFilter = filter(['**/*', '!**/*.html'], { restore: true });
+
+    return gulp.src('_includes/' + '*.html', {base: '.'})
+        .pipe(useref())
+        .pipe(assetsFilter)
+        .pipe(rev())
+        .pipe(assetsFilter.restore)
+        .pipe(revReplace())
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('build', gulpSequence('clean', 'build:main', 'build:post'));
